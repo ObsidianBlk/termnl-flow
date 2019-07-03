de33ea58b6466fe2ac4af60547b4d79687705557
Bryan Miller <bmiller1008@gmail.com>
2019-07-02 20:20:36 -0400

    More work on technoflies

--- source/js/technoflies.js
+++ source/js/technoflies.js
@@ -6,11 +6,29 @@ if (typeof(window.RenderLoop) !== 'undefined'){
     var cnv = null;
     var ctx = null;
 
-    var maxParticleSpeed = 5;
-    var minParticleSpeed = 2;
-    var minParticleDistance = 4;
-    var maxParticleDistance = 10;
+    var minParticleSize = 20;
+    var maxParticleSize = 60;
+    var maxParticleSpeed = 50;
+    var minParticleSpeed = 10;
+    var minParticleDistance = 30;
+    var maxParticleDistance = 80;
+    var maxParticles = 60;
     var particles = [];
+
+    // Set-able variables.
+    var boxSize = 10;
+    var boxSpacing = 2;
+    var boxColor = [0,0,0];
+    var edgeColor = [96,96,96];
+    var glowColor = [255,255,255];
+
+    // Computed variables.
+    var boxCountH = 0;
+    var boxCountV = 0;
+    var renderXOffset = 0;
+    var renderYOffset = 0; 
+
+
 
     function vec2d(){
       this.i = 0;
@@ -30,7 +48,7 @@ if (typeof(window.RenderLoop) !== 'undefined'){
       }
 
       this.lensq = function(){
-        return (i * i) + (j * j);
+        return (this.i * this.i) + (this.j * this.j);
       }
 
       this.length = function(){
@@ -40,38 +58,38 @@ if (typeof(window.RenderLoop) !== 'undefined'){
       this.normalize = function(){
         var l = this.length();
         if (l !== 0){
-          i /= length;
-          j /= length;
+          this.i /= l;
+          this.j /= l;
         }
         return this;
       }
 
       this.invert = function(){
-        i = -i;
-        j = -j;
+        this.i = -this.i;
+        this.j = -this.j;
         return this;
       }
 
       this.add = function(v){
         if (v instanceof vec2d){
-          i += v.i;
-          j += v.j;
+          this.i += v.i;
+          this.j += v.j;
         }
         return this;
       }
 
       this.multv = function(v){
         if (v instanceof vec2d){
-          i *= v.i;
-          j *= v.j;
+          this.i *= v.i;
+          this.j *= v.j;
         }
         return this;
       }
 
       this.mults = function(s){
         if (typeof(s) === "number"){
-          i *= s;
-          j *= s;
+          this.i *= s;
+          this.j *= s;
         }
         return this;
       }
@@ -92,8 +110,12 @@ if (typeof(window.RenderLoop) !== 'undefined'){
 
       if (Math.random() < 0.5){
         this.x = Math.floor(Math.random() * w);
+        if (Math.random() < 0.5)
+          this.y = h;
       } else {
         this.y = Math.floor(Math.random() * h);
+        if (Math.random() < 0.5)
+          this.x = w;
       }
 
       this.s = minParticleSpeed + (Math.random() * (maxParticleSpeed - minParticleSpeed));
@@ -101,6 +123,24 @@ if (typeof(window.RenderLoop) !== 'undefined'){
       this.v.i = (w*0.5) - this.x;
       this.v.j = (h*0.5) - this.y;
       this.v.normalize();
+
+      this.sz = minParticleSize + (Math.random()*(maxParticleSize - minParticleSize));
+
+      this.distanceTo = function(x, y){
+        var dv = (new vec2d()).set(
+          x - this.x,
+          y - this.y
+        );
+        return dv.length();
+      }
+
+      this.strengthAt = function(x, y){
+        var d = this.distanceTo(x,y);
+        if (d <= this.sz){
+          return 1.0 - (d / this.sz);
+        }
+        return 0;
+      }
 
       this.force = function(p){
         if (!this.alive){return;}
@@ -110,9 +150,10 @@ if (typeof(window.RenderLoop) !== 'undefined'){
           p.x - this.x,
           p.y - this.y
         );
-        var d = dv.length;
+        var d = dv.length();
 
         if (d < maxParticleDistance){
+          this.v.mults(this.s);
           dv.normalize();
 
           var f = 0;
@@ -123,6 +164,7 @@ if (typeof(window.RenderLoop) !== 'undefined'){
           } else { // Attract
             f = (d-minParticleDistance)/(maxParticleDistance - minParticleDistance);
           }
+          f *= 0.1; // Weaken the force by a lot
           dv.mults(f * this.s); // Push away/toward at some fraction of the particles speed.
           this.v.add(dv);
         }
@@ -132,10 +174,9 @@ if (typeof(window.RenderLoop) !== 'undefined'){
         if (!this.alive){return;}
 
         this.v.normalize();
-        var dx = this.v.i * this.s * delta;
-        var dy = this.v.j * this.s * delta;
-        this.x += dx;
-        this.y += dy;
+        var dv = this.v.clone().mults(this.s * delta);
+        this.x += dv.i;
+        this.y += dv.j;
 
         if (this.x < 0 || this.y < 0 || this.x > w || this.y > h)
           this.alive = false;
@@ -146,17 +187,143 @@ if (typeof(window.RenderLoop) !== 'undefined'){
     // -------------------------------------------------
     // ------------------------------------------------- 
 
-    function Render(delta){
+
+    // ---------------
+    // NOTE: The next three functions were borrowed from...
+    // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    function componentToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    } 
+
+    function hexToRgb(hex) {
+      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+      });
+
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null;
+    }
+    // -------------
+    // Back to my stuff!!
+    
+    function rgbArrToHex(rgb){
+      return rgbToHex(rgb[0], rgb[1], rgb[2]);
+    }
+    
+
+    function ColorAtPoint(x, y){
+      var str = 0;
+      particles.forEach((p) => {
+        str += p.strengthAt(x,y);
+      });
+      str = Math.min(1.0, str);
+      var istr = 1.0 - str;
+      return [
+        Math.min(255, Math.floor((edgeColor[0] * istr) + (glowColor[0] * str))),
+        Math.min(255, Math.floor((edgeColor[1] * istr) + (glowColor[1] * str))),
+        Math.min(255, Math.floor((edgeColor[2] * istr) + (glowColor[2] * str)))
+      ];
+    }
+
+    function RenderLine(x1, y1, x2, y2, c1, c2){
+      ctx.save();
+
+      var grad = ctx.createLinearGradient(x1, y1, x2, y2);
+      grad.addColorStop(0, c1);
+      grad.addColorStop(1, c2);
+
+      ctx.strokeStyle = grad;
+      ctx.beginPath();
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+
+      ctx.stroke();
+
+      ctx.restore();
+    }
+
+    function RenderBox(x, y){
+      ctx.save();
+      ctx.fillStyle = rgbToHex(boxColor[0], boxColor[1], boxColor[2]);
+      ctx.fillRect(x + boxSpacing, y + boxSpacing, boxSize, boxSize);
+      ctx.restore();
+
+      var xl = x + boxSpacing;
+      var xr = xl + boxSize;
+      var yt = y + boxSpacing;
+      var yb = yt + boxSize;
+
+      var ctl = rgbArrToHex(ColorAtPoint(xl, yt));
+      var ctr = rgbArrToHex(ColorAtPoint(xr, yt));
+      var cbr = rgbArrToHex(ColorAtPoint(xr, yb));
+      var cbl = rgbArrToHex(ColorAtPoint(xl, yb));
+
+      RenderLine(xl, yt, xr, yt, ctl, ctr);
+      RenderLine(xr, yt, xr, yb, ctr, cbr);
+      RenderLine(xr, yb, xl, yb, cbr, cbl);
+      RenderLine(xl, yb, xl, yt, cbl, ctl);
+    }
+
+    function Update(delta){
       if (!running || cnv === null || ctx === null){return;}
 
-      // Drawing Operations go here!!!
+      if (cnv.width !== cnv.clientWidth || cnv.height !== cnv.clientHeight){
+        cnv.width = cnv.clientWidth;
+        cnv.height = cnv.clientHeight;
+        CalculateBoxRendering();
+      }
+
+      while (particles.length < maxParticles)
+        particles.push(new particle(cnv.width, cnv.height));
+      for (let i=0; i < particles.length; i++){
+        for (let j=i+1; j < particles.length; j++){
+          particles[i].force(particles[j]);
+          particles[j].force(particles[i]);
+        }
+      }
+      particles.forEach((p)=>{p.update(delta);});
+      particles = particles.filter((p)=>{return p.alive;});
+
+      ctx.clearRect(0,0,cnv.width, cnv.height);
+
+      var bs = boxSize + (2 * boxSpacing);
+      var y = renderYOffset;
+
+      for (let j=0; j < boxCountV; j++){
+        var x = renderXOffset
+        for (let i=0; i < boxCountH; i++){
+          RenderBox(x, y);
+          x += bs;
+        }
+        y += bs;
+      }
     }
-    window.RenderLoop.register(Render);
+    window.RenderLoop.register(Update);
 
 
 
     // -------------------------------------------------
     // -------------------------------------------------
+    
+
+    function CalculateBoxRendering(){
+      var bs = boxSize + (2 * boxSpacing);
+      boxCountH = Math.floor(cnv.width / bs);
+      boxCountV = Math.floor(cnv.height / bs);
+      renderXOffset = Math.floor((cnv.width - (bs * boxCountH)) * 0.5);
+      renderYOffset = Math.floor((cnv.height - (bs * boxCountV)) * 0.5);
+    }
     
     var TechnoFlies = {
       start: function(){
@@ -171,12 +338,24 @@ if (typeof(window.RenderLoop) !== 'undefined'){
       isRunning: function(){return running;},
 
       setCanvas: function(name, isclass){
-        if (cnv !== null){
-          TechnoFlies.stop();
-          cnv = null;
-          ctx = null;
+        var _cnv = document.querySelector("canvas" + (((isclass) ? "." : "#") + name));
+        if (_cnv){
+          ctx = _cnv.getContext("2d");
+          if (ctx){
+            let restart = false;
+            if (cnv !== null){
+              TechnoFlies.stop();
+              restart = true;
+            }
+            cnv = _cnv;
+            if (restart)
+              TechnoFlies.start();
+          } else {
+            ctx = null;
+            if (cnv !== null)
+              ctx = cnv.getContext("2d");
+          }
         }
-        // Do a query selection to get the canvas!
       }
     };
 
